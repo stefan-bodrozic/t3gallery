7bdb8c58f83d6e5780637d9e37bd088e2765f17d
Stefan Bodrozic <s.bodrozic@vegait.rs>
2024-05-17 16:16:43 +0200

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic"
 
 const mockUrls = [
   "https://utfs.io/f/f97efddc-fd66-40ed-b0a6-aacef99f63d8-jlo1ag.png",
@@ -11,13 +14,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany()
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>
+            {post.name}
+          </div>
+        ))}
         {
-          [...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
-            <div key={image.id} className="w-48 p-4">
+          [...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+            <div key={image.id + '-' + index} className="w-48 p-4">
               <img src={image.url} alt="image" />
             </div>
           ))
