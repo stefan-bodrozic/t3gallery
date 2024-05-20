83fcfc4c139c80464736f462e7346572ff2de478
Stefan Bodrozic <s.bodrozic@vegait.rs>
2024-05-20 11:35:24 +0200

    uploadthing added

--- README.md
+++ README.md
@@ -7,8 +7,10 @@
 - [x] Tidy up build process
 - [x] Actually set up a database (vercel postgres)
 - [x] Attach database to UI
-- [ ] Add authentication (w/ clerk)
+- [x] Add authentication (w/ clerk)
 - [ ] Add image upload
+- [ ] "taint" (server-only)
+- [ ] Use Next/Image component
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
 - [ ] Delete button (w/ Server Actions)
--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,15 +1,24 @@
+"use client"
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+    const router = useRouter()
+
     return (
       <nav className="flex items-center justify-between border-b p-4 text-xl font-semibold bo">
         <div>Gallery</div>
   
-        <div>
+        <div className="flex flex-row">
             <SignedOut>
                 <SignInButton />
             </SignedOut>
             <SignedIn>
+                <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {
+                  router.refresh()
+                }}/>
                 <UserButton />
             </SignedIn>
         </div>
