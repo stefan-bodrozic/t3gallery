130d6a5c0dd6ae3bb288f823b170ef3ce586e42c
Stefan Bodrozic <s.bodrozic@vegait.rs>
2024-05-22 10:37:09 +0200

    move to grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,16 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider> 
-      <html lang="en" className={`${GeistSans.variable} flex flex-gap gap-4`}>
+      <html lang="en" className={`${GeistSans.variable}`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
           <body>
-            <TopNav />
-            {children}
+            <div className="h-screen grid grid-rows-[auto, 1fr]">
+              <TopNav />
+              <main className="overflow-y-scroll">
+                {children}
+              </main>
+            </div>
+
             {modal}
             <div id="modal-root"/>
           </body>
